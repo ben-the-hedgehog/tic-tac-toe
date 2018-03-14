c95b7658abe25f4688c0ef96acaf18cbd04b8ffd
Ben Pang <benpang@outlook.com>
2018-03-14 12:38:22 -0400

    choices for game status code

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,7 +1,18 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User
 
+GAME_STATUS_CHOICES = (
+    ('F', "first player to move"),
+    ('S', "second player to move"),
+    ('W', "first player wins"),
+    ('L', "second player wins"),
+    ('D', "draw")
+)
+
 # Create your models here.
+@python_2_unicode_compatible
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player",
         on_delete=models.CASCADE)
@@ -10,7 +21,11 @@ class Game(models.Model):
 
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, default='F')
+    status = models.CharField(max_length=1, choices=GAME_STATUS_CHOICES, default='F')
+    move_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{0} vs. {1}, turn {2}".format(self.first_player, self.second_player, self.move_count)
 
 class Move(models.Model):
     x = models.IntegerField()
