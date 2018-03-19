1c193a12bec064925f3b33464a523e445d5582ad
Ben Pang <benpang@outlook.com>
2018-03-19 02:04:25 -0400

    added home template

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Q
+from gameplay.models import Game
 
 # Create your views here.
 def home(request):
-    return render(request, "player/home.html")
+    games_first_player = Game.objects.filter(
+        first_player = request.user,
+        status = 'F'
+    )
+
+    games_second_player = Game.objects.filter(
+        second_player = request.user,
+        status = 'S'
+    )
+
+    all_my_games = list(games_first_player) + list(games_second_player)
+
+    return render(request, "player/home.html",
+        {
+            'ngames' : Game.objects.count(),
+            'nactivegames' : Game.objects.filter(Q(status='F') | Q(status='S')).count(),
+            'allmygames' : all_my_games
+        })
--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import welcome, test
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome),
     path('test/', test),
+    path('player/', include('player.urls'))
 ]
